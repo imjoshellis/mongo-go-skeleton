48193571f099ba56d6aebc289e29e69c3ddc2b09
Josh Ellis <josh@imjoshellis.com>
2020-11-28 19:44:18 -0600

    Update main.go

--- src/main.go
+++ src/main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func main() {
@@ -15,18 +16,29 @@ func main() {
 	var err error
 	user.Username = "imjoshellis"
 	user.Email = "josh@imjoshlis.com"
-
+	if user.ID == primitive.NilObjectID {
+		log.Info("User has nil ObjectID. Attempting to save...")
+	}
 	err = user.Save()
 	if err != nil {
 		log.Fatal("Error reading user from db: %v", err)
 	}
-	log.Info("User successfully saved to db.")
+	if user.ID != primitive.NilObjectID {
+		log.Info("User has a generated ObjectID. Save was successful.")
+	}
 
-	err = user.Get()
+	var readUser entities.User
+	readUser.ID = user.ID
+	if readUser.Email == "" {
+		log.Info("New user entity created with matching ID. Attempting to read...")
+	}
+	err = readUser.Get()
 	if err != nil {
 		log.Fatal("Error reading user from db: %v", err)
 	}
-	log.Info("User successfully read from db.")
+	if readUser.Email == user.Email {
+		log.Info("New user has the same email as original. Read was successful.")
+	}
 
 	defer func() {
 		log.Println("Disconnecting from MongoDB...")
